b5f1307cc8b39d6cb63ee0e06a2f2087acd13104
James Lukanta <james.lukanta@gmail.com>
2017-01-11 17:35:50 -0800

    master: Added error checking for SecRandomCopyBytes return value

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -522,7 +522,9 @@ static __strong NSData *CRLFCRLF;
     CFHTTPMessageSetHeaderFieldValue(request, CFSTR("Host"), (__bridge CFStringRef)(_url.port ? [NSString stringWithFormat:@"%@:%@", _url.host, _url.port] : _url.host));
         
     NSMutableData *keyBytes = [[NSMutableData alloc] initWithLength:16];
-    SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
+    if (SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes) == -1) {
+        [self _failWithError:[NSError errorWithDomain:SRWebSocketErrorDomain code:2134 userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithFormat:@"Invalid SecRandomCopyBytes return value"] forKey:NSLocalizedDescriptionKey]]];
+    }
     _secKey = [keyBytes SR_stringByBase64Encoding];
     assert([_secKey length] == 24);
     
@@ -1353,7 +1355,9 @@ static const size_t SRFrameHeaderOverhead = 32;
         }
     } else {
         uint8_t *mask_key = frame_buffer + frame_buffer_size;
-        SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), (uint8_t *)mask_key);
+        if (SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), (uint8_t *)mask_key) == -1) {
+            [self _failWithError:[NSError errorWithDomain:SRWebSocketErrorDomain code:2134 userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithFormat:@"Invalid SecRandomCopyBytes return value"] forKey:NSLocalizedDescriptionKey]]];
+        }
         frame_buffer_size += sizeof(uint32_t);
         
         // TODO: could probably optimize this with SIMD
